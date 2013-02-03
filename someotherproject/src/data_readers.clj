eadbc377ac789535b874332b37d858d92efdb9be
Steve Suehs <skelter.net@gmail.com>
2013-02-03 13:48:53 -0600

    mock out the reader spec

---
+++ someotherproject/src/data_readers.clj
@@ -0,0 +1,4 @@
+;; This is read by Clojure reader and allows us to specify what function is used to create the tag
+;{user/NasFile          someotherproject.core/readNasFile
+; nastimer/NasFile      someotherproject.core/readNasFile
+; nastimer.core/NasFile someotherproject.core/readNasFile }
