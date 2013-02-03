eca8c0a9a610f57c5dcf6cee812edcd6e14ca731
Steve Suehs <skelter.net@gmail.com>
2013-02-03 12:29:39 -0600

    prelim work on example

--- nastimer/src/nastimer/core.clj
+++ nastimer/src/nastimer/core.clj
@@ -1,6 +1,37 @@
 (ns nastimer.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord NasFile [path size])
+
+(defn directory? [file]
+  (.isDirectory file))
+
+(defn normal? [file]
+  (not (.isDirectory file)))
+
+(defn nasfile [file]
+     (NasFile. (.getPath file) (.length file)))
+
+(defn build-nasfiles
+  "Return a sequence of NasFiles from a given directory, expecting a java.io.File ."
+  [filedir]
+  (map nasfile (filter normal? (.listFiles filedir))))
+
+(defn save-nasfiles
+  "Save a list of nasfiles to a text file."
+  [nasfiles outputfile]
+  (binding [*out* (java.io.FileWriter. outputfile)]
+    (for [nf nasfiles]
+      (pprint nf *out*))))
+
+(defn load-nasfiles
+  "Load a list of nasfiles from a text file."
+  [inputfilename]
+  (binding [*in* (java.io.PushbackReader. (java.io.FileReader. inputfilename))]
+    (read)))
+
+       
+(defn scan
+  "Returns a sequence of NasFiles as read from rootToScan."
+  [rootToScan]
+  (let [root (java.io.File. rootToScan)]
+    (conj (buid-nasfiles root) (map build-nasfiles (subfolders root)))
