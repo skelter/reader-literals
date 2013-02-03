7c8a7df8ab60c34f2b451a40097a747e8c088985
Steve Suehs <skelter.net@gmail.com>
2013-02-03 14:25:38 -0600

    Correct given the example file

--- someotherproject/src/data_readers.clj
+++ someotherproject/src/data_readers.clj
@@ -1,4 +1,5 @@
 ;; This is read by Clojure reader and allows us to specify what function is used to create the tag
-;{user/NasFile          someotherproject.core/readNasFile
-; nastimer/NasFile      someotherproject.core/readNasFile
-; nastimer.core/NasFile someotherproject.core/readNasFile }
+{
+ nasfile/file          someotherproject.core/readNasFile
+  }
+
