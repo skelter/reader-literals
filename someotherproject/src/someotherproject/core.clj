271360d3ef1a7ebd42ff42135744802d5700ba36
Steve Suehs <skelter.net@gmail.com>
2013-02-09 21:34:15 -0600

    We tried, on a lark, to see what happens when we specify a macro as the reader function.

--- someotherproject/src/someotherproject/core.clj
+++ someotherproject/src/someotherproject/core.clj
@@ -1,8 +1,7 @@
 (ns someotherproject.core)
 
-(defn readNasFile [map]
-  map)
-
+(defmacro readNasFile [mysym]
+   `(fn [s#  ] (print ~mysym s# )))
 
 (defn load-filelist
   "Load a list of files from the first example."
@@ -10,3 +9,4 @@
   (with-open [r (java.io.PushbackReader. (java.io.FileReader. inputfilename))]
     (doall (take-while identity (repeatedly (fn [] (read r false nil)))))))
 
+;(load-filelist "../myfilelist.txt")
